b1e2d6ceba1ae7091358b629897e1a59f17de5a1
hbyx.zpf <hbyx.zpf@gmail.com>
2022-01-07 11:17:19 +0800

    fix bugs

---
+++ src/Tools/NewWeiBo.php
@@ -0,0 +1,34 @@
+<?php
+declare (strict_types=1);
+
+namespace Smalls\VideoTools\Tools;
+
+use Smalls\VideoTools\Interfaces\IVideo;
+use Smalls\VideoTools\Logic\NewWeiBoLogic;
+
+/**
+ * Created By 1
+ * Author：smalls
+ * Email：smalls0098@gmail.com
+ * Date：2020/6/13 - 21:04
+ **/
+class NewWeiBo extends Base implements IVideo
+{
+
+    /**
+     * 更新时间：2020/7/31
+     * @param string $url
+     * @return array
+     */
+    public function start(string $url): array
+    {
+        $obj         = new NewWeiBoLogic($this, 'newweibo');
+        $this->logic = $obj;
+        $this->logic->setOriginalUrl($url);
+        $this->logic->checkUrlHasTrue();
+        $this->logic->setFid();
+        $this->logic->setMid();
+        $this->logic->setContents();
+        return $this->exportData();
+    }
+}
